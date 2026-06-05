71c974a58e95ee8c6619d3c8dda3ab80d4f80a0d
HireAtPoint <hireatpointllp@gmail.com>
2026-06-05 15:29:24 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const VERIFY_TOKEN = 'mybot123';
+const MAKE_WEBHOOK_URL = 'https://hook.us2.make.com/x1amyisard3ektrw6asghb2lanp4fert';
+
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    console.log('Webhook verified!');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', (req, res) => {
+  const body = req.body;
+  if (body.object === 'whatsapp_business_account') {
+    fetch(MAKE_WEBHOOK_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body)
+    });
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.get('/', (req, res) => res.send('WhatsApp Webhook is running!'));
+
+app.listen(3000, () => console.log('Server running on port 3000'));
